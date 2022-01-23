d1b3a48fef236b8cdcad51e790bfc5584594dae8
alyesque <89320257+alyesque@users.noreply.github.com>
2022-01-22 17:15:27 -0800

    bug fix

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const videos = require("./routes/video.js");
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 5000000 })
